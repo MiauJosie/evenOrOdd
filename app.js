28fb99602002f45dc9ef7c8069223f20a1346004
Victória Josie <miautoofu@gmail.com>
2022-09-28 16:11:23 -0300

    Minor changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const num = Number(window.prompt('evenOrOdd'))
 
 if(Number.isNaN(num)) {
-    alert("O valor inserido não é um número válido.")
+    alert("The inserted value is not a valid number")
     throw new Error()
 }
 
@@ -10,10 +10,10 @@ let even = false
 function evenOrOdd(num){
     if(num % 2 === 0){
         even = true
-        document.write("O número é par")
+        document.write(`${num} is even`)
         return
     }
-    document.write("O número é ímpar")
+    document.write(`${num} is odd`)
 }
 
 evenOrOdd(num)
